a661283f7249ebdc208af2269f96ec45ca05ed5c
George Cristian <george.crist5@gmail.com>
2023-04-15 15:58:01 +0200

    Added SQLAlchemy and a Users model

--- analytics_backend/__init__.py
+++ analytics_backend/__init__.py
@@ -1,5 +1,10 @@
+import os
 from flask import Flask
+from .extensions import db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URI']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
 from analytics_backend import views
